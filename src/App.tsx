091c52dd645454635e48a3911fefe88bfb1d020b
Prakash Shrestha <prakashrestha2075@gmail.com>
2025-03-22 19:26:44 +0545

    ✨ Feat: landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <p className="text-red-400"></p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
